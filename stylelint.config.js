37d2fc313b2022aaed38fe918fffd1e22db4fdd7
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2021-02-08 10:34:01 -0600

    fix(rules): disable font-weight-notation

--- stylelint.config.js
+++ stylelint.config.js
@@ -71,7 +71,7 @@ module.exports = {
 		'font-family-name-quotes':                            'always-unless-keyword',
 		'font-family-no-duplicate-names':                     true,
 		'font-family-no-missing-generic-family-keyword':      [true, { severity: 'warning' }],
-		'font-weight-notation':                               ['numeric', { ignore: ['relative'] }],
+		'font-weight-notation':                               0,
 		'function-blacklist':                                 null,
 		'function-calc-no-invalid':                           true,
 		'function-calc-no-unspaced-operator':                 true,
